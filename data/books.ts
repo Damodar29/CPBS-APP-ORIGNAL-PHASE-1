2b89be9105eff54003bf1bbc97b6ba94173bfca2
Damodar29 <80112163+Damodar29@users.noreply.github.com>
2026-01-25 10:12:34 +0530

    feat: Add Google Drive URLs for bhajan books
    Includes direct download links to PDF files hosted on Google Drive. This enables users to access and read bhajan books offline.

--- data/books.ts
+++ data/books.ts
@@ -2,22 +2,112 @@
 import { Book } from '../types';
 
 export const BOOKS_DATA: Book[] = [
-  { id: '16', title: 'श्री प्रेम भक्ति चंद्रिका', fileName: 'श्री प्रेम भक्ति चंद्रिका.pdf', url: '' },
-  { id: '1', title: 'श्री गौरपार्षद एवं गौडीय वैष्णव-आचार्यों', fileName: 'श्री गौरपार्षद एवं गौडीय वैष्णव-आचार्यों.pdf', url: '' },
-  { id: '2', title: 'अष्टकावली', fileName: 'अष्टकावली.pdf', url: '' },
-  { id: '3', title: 'उपदेशामृत', fileName: 'उपदेशामृत.pdf', url: '' },
-  { id: '4', title: 'गीत रसामृत', fileName: 'गीत रसामृत.pdf', url: '' },
-  { id: '5', title: 'गुरुदेव श्री विरही जी महाराज जीवन चरित्र', fileName: 'गुरुदेव श्री विरही जी महाराज जीवन चरित्र.pdf', url: '' },
-  { id: '6', title: 'गौरांग भजन माला भाग 3', fileName: 'गौरांग भजन माला भाग 3.pdf', url: '' },
-  { id: '7', title: 'गौरांग भजन माला', fileName: 'गौरांग भजन माला.pdf', url: '' },
-  { id: '8', title: 'गौड़ीय गीति गुच्छ', fileName: 'गौड़ीय गीति गुच्छ.pdf', url: '' },
-  { id: '9', title: 'दिगदर्शिका', fileName: 'दिगदर्शिका.pdf', url: '' },
-  { id: '10', title: 'नाम रसामृत', fileName: 'नाम रसामृत.pdf', url: '' },
-  { id: '11', title: 'प्रेम रसामृत', fileName: 'प्रेम रसामृत.pdf', url: '' },
-  { id: '12', title: 'भजन गीति', fileName: 'भजन गीति.pdf', url: '' },
-  { id: '13', title: 'भजन संकलन भूषण', fileName: 'भजन संकलन भूषण.pdf', url: '' },
-  { id: '14', title: 'शिक्षाष्टकम्', fileName: 'शिक्षाष्टकम्.pdf', url: '' },
-  { id: '15', title: 'श्री गौर पार्षद भाग 2', fileName: 'श्री गौर पार्षद भाग 2.pdf', url: '' },
-  { id: '17', title: 'श्रीमद भगवद्गीता यथारूप', fileName: 'श्रीमद भगवद्गीता यथारूप.pdf', url: '' },
-  { id: '18', title: 'सपार्षद गौर महिमा', fileName: 'सपार्षद गौर महिमा.pdf', url: '' },
+  { 
+    id: '16', 
+    title: 'श्री प्रेम भक्ति चंद्रिका', 
+    fileName: 'श्री प्रेम भक्ति चंद्रिका.pdf', 
+    url: 'https://drive.google.com/file/d/1nkO7moHZd6uVOH3L0HS6zuNAaEb_is0B/view?usp=sharing' 
+  },
+  { 
+    id: '1', 
+    title: 'श्री गौरपार्षद एवं गौडीय वैष्णव-आचार्यों', 
+    fileName: 'श्री गौरपार्षद एवं गौडीय वैष्णव-आचार्यों.pdf', 
+    url: 'https://drive.google.com/file/d/1E3V8RcwedaWm-nMKWnhjZflkRU0ikpti/view?usp=drive_link' 
+  },
+  { 
+    id: '2', 
+    title: 'अष्टकावली', 
+    fileName: 'अष्टकावली.pdf', 
+    url: 'https://drive.google.com/file/d/1eTLa9XT85W7nZ-0yFuuMvc2rXUpIaiT-/view?usp=drive_link' 
+  },
+  { 
+    id: '3', 
+    title: 'उपदेशामृत', 
+    fileName: 'उपदेशामृत.pdf', 
+    url: 'https://drive.google.com/file/d/1o7GyR5nB1_5dStg61zEuZMG21VzRibjz/view?usp=drive_link' 
+  },
+  { 
+    id: '4', 
+    title: 'गीत रसामृत', 
+    fileName: 'गीत रसामृत.pdf', 
+    url: 'https://drive.google.com/file/d/1RwDL9da_tZCiNs5csg_EYLnBxyK3vyFU/view?usp=drive_link' 
+  },
+  { 
+    id: '5', 
+    title: 'गुरुदेव श्री विरही जी महाराज जीवन चरित्र', 
+    fileName: 'गुरुदेव श्री विरही जी महाराज जीवन चरित्र.pdf', 
+    url: 'https://drive.google.com/file/d/1TvPu0nnNWk8WKH88TJJa-MYU0hbDcq2q/view?usp=drive_link' 
+  },
+  { 
+    id: '6', 
+    title: 'गौरांग भजन माला भाग 3', 
+    fileName: 'गौरांग भजन माला भाग 3.pdf', 
+    url: 'https://drive.google.com/file/d/1a2KthvrokOfJ8yiR_Pm_RQja2_9isFbi/view?usp=drive_link' 
+  },
+  { 
+    id: '7', 
+    title: 'गौरांग भजन माला', 
+    fileName: 'गौरांग भजन माला.pdf', 
+    url: 'https://drive.google.com/file/d/1B9EcKW8yp0Z1abaOThpy12Iv7WOikeF9/view?usp=drive_link' 
+  },
+  { 
+    id: '8', 
+    title: 'गौड़ीय गीति गुच्छ', 
+    fileName: 'गौड़ीय गीति गुच्छ.pdf', 
+    url: 'https://drive.google.com/file/d/17JizBU2G7gz2LFDJ69g7CvtADoQjYab1/view?usp=drive_link' 
+  },
+  { 
+    id: '9', 
+    title: 'दिगदर्शिका', 
+    fileName: 'दिगदर्शिका.pdf', 
+    url: 'https://drive.google.com/file/d/1N8wasyTXxEwAVVxfJmgvRyzYrJsOCvtp/view?usp=drive_link' 
+  },
+  { 
+    id: '10', 
+    title: 'नाम रसामृत', 
+    fileName: 'नाम रसामृत.pdf', 
+    url: 'https://drive.google.com/file/d/1QDG4UW20MhJ_dvXlW7VykxonhoITkihx/view?usp=drive_link' 
+  },
+  { 
+    id: '11', 
+    title: 'प्रेम रसामृत', 
+    fileName: 'प्रेम रसामृत.pdf', 
+    url: 'https://drive.google.com/file/d/1LztKnaXMnmIwn0ztMdZ7BCh8JqGOmWVn/view?usp=drive_link' 
+  },
+  { 
+    id: '12', 
+    title: 'भजन गीति', 
+    fileName: 'भजन गीति.pdf', 
+    url: 'https://drive.google.com/file/d/1qGMpe6JoHfIBFXj4L4EmPRu7oQkbWcQR/view?usp=drive_link' 
+  },
+  { 
+    id: '13', 
+    title: 'भजन संकलन भूषण', 
+    fileName: 'भजन संकलन भूषण.pdf', 
+    url: 'https://drive.google.com/file/d/1a2KthvrokOfJ8yiR_Pm_RQja2_9isFbi/view?usp=drive_link' 
+  },
+  { 
+    id: '14', 
+    title: 'शिक्षाष्टकम्', 
+    fileName: 'शिक्षाष्टकम्.pdf', 
+    url: 'https://drive.google.com/file/d/15_2OuwHE9LNVs8rbOjRIaF_zQNg26kc_/view?usp=drive_link' 
+  },
+  { 
+    id: '15', 
+    title: 'श्री गौर पार्षद भाग 2', 
+    fileName: 'श्री गौर पार्षद भाग 2.pdf', 
+    url: 'https://drive.google.com/file/d/1JSj18hh0Jja3Ox36QbEKnpuOhmqZAZcw/view?usp=drive_link' 
+  },
+  { 
+    id: '17', 
+    title: 'श्रीमद भगवद्गीता यथारूप', 
+    fileName: 'श्रीमद भगवद्गीता यथारूप.pdf', 
+    url: 'https://drive.google.com/file/d/1P9NM2iYDcn-_wBEscr0QaMFU-nLDd0jR/view?usp=drive_link' 
+  },
+  { 
+    id: '18', 
+    title: 'सपार्षद गौर महिमा', 
+    fileName: 'सपार्षद गौर महिमा.pdf', 
+    url: 'https://drive.google.com/file/d/1FO1zwEha2b1D-ON5D9ybR2TqNh2FL5HR/view?usp=drive_link' 
+  },
 ];
